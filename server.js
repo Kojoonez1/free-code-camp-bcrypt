6d5e04366a45640df8283297621267be8f61228b
Kojoonez1 <marfoarthur@gmail.com>
2024-07-20 09:25:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,22 +8,13 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-    /*Store hash in your db*/
-  });
-bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-    /*res == true or false*/
-  });
-bcrypt.hash('passw0rd!', 13, (err, hash) => {
-    console.log(hash);
-    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-bcrypt.compare('passw0rd!', hash, (err, res) => {
-      console.log(res); //true
-    });
-  });
+
+
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash + "<== this the bcrpt hash")
+  });
 
 //END_ASYNC
 
