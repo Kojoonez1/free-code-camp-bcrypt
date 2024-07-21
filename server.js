9965f6fea6b6170292a17dd08d3ff3c7b51d8674
Kojoonez1 <marfoarthur@gmail.com>
2024-07-21 21:15:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,9 +23,11 @@ const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err,hash) => {
 //END_ASYNC
 
 //START_SYNC
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
 
-
-
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
 
 
